7b57673889c1369770e30622bb08a6bb5baaf151
Kevin Miller <kemiller@csumb.edu>
2013-11-19 08:45:17 -0800

    Added WCAG status and orphaned tests.

--- guidelines/wcag/wcag.js
+++ guidelines/wcag/wcag.js
@@ -2,15 +2,16 @@
 		$(document).ready(function() {
 			$.getJSON('https://rawgithub.com/kevee/quail/yaml-config/dist/tests.json', function(tests) {
 				$.getJSON('https://rawgithub.com/kevee/quail/yaml-config/dist/guidelines/wcag.json', function(guideline) {
-					$.each(guideline.guideline, function(sc, id) {
-						$.each(sc.techniques, function(technique) {
+					$.each(guideline.guidelines, function(id, sc) {
+						$.each(sc.techniques, function(techniqueId, technique) {
 							var $tr = $('<tr>');
 							$tr.append('<td><strong>' + id + '</strong> ' + sc.title + '</td>');
 							$tr.append('<td><strong>' + technique + '</strong> ' + guideline.techniques[technique].description + '</td>');
 							if(typeof guideline.techniques[technique].tests !== 'undefined') {
 								$ul = $('<ul>');
-								$.each(guideline.techniques[technique].tests, function(test) {
-									$ul.append('<li>' + test + '</li>');
+								$.each(guideline.techniques[technique].tests, function(testId, test) {
+									$ul.append('<li><a href="http://quail.readthedocs.org/en/latest/tests/"' + test +'.html">' + test + '</a></li>');
+									delete tests[test];
 								});
 								$tr.append('<td>' + $ul.html() + '</td>');
 							}
@@ -19,6 +20,10 @@
 							}
 							$('#guideline tbody').append($tr);
 						});
+					});
+					$('#loading').remove();
+					$.each(tests, function(testId, test) {
+						$('#orphans').append('<li>' + testId + '</li>');
 					});
 				});
 			});
