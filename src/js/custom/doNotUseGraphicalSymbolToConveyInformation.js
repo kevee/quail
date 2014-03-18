90a98191659f1c17077e980ec26329906dc47c8d
Peter Droogmans <peter@attiks.com>
2014-03-18 22:19:28 +0100

    white/black list added, support for abbr and accronym added

--- src/js/custom/doNotUseGraphicalSymbolToConveyInformation.js
+++ src/js/custom/doNotUseGraphicalSymbolToConveyInformation.js
@@ -1,19 +1,24 @@
 quail.doNotUseGraphicalSymbolToConveyInformation = function() {
-  quail.html.find(quail.textSelector).each(function() {
-    // @todo: add a list of allowed values.
+  quail.html.find(quail.textSelector + ':not(abbr, acronym)').each(function() {
+    var whiteList = '✓';
+    var blackList = '?xo[]()+-!*xX';
+
     var text = $(this).text();
-    if (text.length === 1) {
-      // Check regularly used single character symbols.
-      if ('?xo[]()+-!*xX'.indexOf(text) >= 0) {
+
+    // @todo add support for other languages.
+    // Remove all alphanumeric characters.
+    var textLeft = text.replace(/[\W\s]+/g, '');
+    // If we have an empty string something is wrong.
+    if (textLeft.length === 0) {
+      // Unless if it's white listed.
+      if (whiteList.indexOf(text) === -1) {
         quail.testFails('doNotUseGraphicalSymbolToConveyInformation', $(this));
       }
     }
-    else {
-      // @todo add support for other languages.
-      // Remove all alphanumeric characters.
-      var textLeft = text.replace(/[\W\s]+/g, '');
-      // If we have an empty string something is wrong.
-      if (textLeft.length === 0) {
+
+    if (text.length === 1) {
+      // Check regularly used single character symbols.
+      if (blackList.indexOf(text) >= 0) {
         quail.testFails('doNotUseGraphicalSymbolToConveyInformation', $(this));
       }
     }
