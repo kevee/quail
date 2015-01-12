61e630766530415937f98fa2e2ab757ad05fa192
Christopher Baldwin <c1.baldwin@qut.edu.au>
2015-01-12 16:46:56 +1000

    Revert "Allow plugin to be used as an AMD module: https://github.com/umdjs/umd"
    This reverts commit e5e2e6f79fca379d4711f17204a980cec5cc66ab.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,19 +30,8 @@ module.exports = function(grunt) {
     },
     concat: {
       options: {
-        banner: [
-          "<%= pkg.options.banner %>",
-          "!function(root, factory) {",
-          "  if (typeof define === 'function' && define.amd) {",
-          "    define(['jquery'], factory);",
-          "  } else {",
-          "    factory(root.jQuery);",
-          "  }",
-          "}(this, function($) {",
-          "  'use strict';",
-          "  var jQuery = $;"
-          ].join("\n"),
-        footer: "\n" + '});',
+        banner: '<%= pkg.options.banner %>' + "\n" + ';(function($) {' + "\n" + '\'use strict\';' + '\n',
+        footer: "\n" + '})(jQuery);',
         stripBanners: true
       },
       dist: {
